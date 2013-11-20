6df6e40d5a508cef87284ba7318864e77e116a73
Katja Hebestreit <katja.hebestreit@uni-muenster.de>
2013-11-20 18:39:58 +0000

    can handle CompressedCharacterList in elementMetadata column
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiSeq@83460 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/annotateGRanges.R
+++ R/annotateGRanges.R
@@ -21,14 +21,29 @@
       ind.many <- as.numeric(as.character(tab$Var1[ind]))
       matches.one <- matches[!is.element(matches$query, ind.many), ]
 
-      helper[matches.one$query] <- unlist(elementMetadata(regions)[, regionInfo])[matches.one$subject]
+      ids <- elementMetadata(regions)[, regionInfo]
+      
+      if(is(ids, "CompressedCharacterList")){
+        ids.l <- sapply(ids, length)
+        ids.char <- character(length=length(ids))
+        ids.l.1 <- which(ids.l == 1)
+        ids.l.n <- which(ids.l > 1)
+        if(length(ids.l.1) > 1){
+          ids.char[ids.l.1] <- unlist(ids[ids.l.1])
+        }
+        if(length(ids.l.n) > 1){
+          ids.char[ids.l.n] <- sapply(ids[ids.l.n], function(x) paste(x, collapse = ","))
+        }
+        ids <- ids.char
+      } 
+      helper[matches.one$query] <- ids[matches.one$subject]
       
       for(i in ind.many){
         ind.reg <- matches$subject[matches$query == i]
-        names.reg <- unlist(elementMetadata(regions)[,regionInfo][ind.reg])
+        names.reg <- ids[ind.reg]
         names.reg <- sort(unique(names.reg))
-        ids <- paste(names.reg, collapse=",")
-        helper[i] <- ids
+        ids.i <- paste(names.reg, collapse=",")
+        helper[i] <- ids.i
       }
     }
     
